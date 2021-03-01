4a50d7d4929a65d376d00770ebd01dcbab6b2906
gissandrogama <gissandrogama@gmail.com>
2021-03-01 00:23:46 -0300

    chore(schema): add associations

--- lib/contracts_api/legal_entyti/company.ex
+++ lib/contracts_api/legal_entyti/company.ex
@@ -2,6 +2,7 @@ defmodule ContractsApi.LegalEntyti.Company do
   use Ecto.Schema
   import Ecto.Changeset
   alias ContractsApi.Superscription.Address
+  alias ContractsApi.Covenant.Contract
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -10,6 +11,7 @@ defmodule ContractsApi.LegalEntyti.Company do
     field :cnpj, :string
 
     has_many :addresses, Address
+    has_many :contracts, Contract
 
     timestamps()
   end
@@ -19,6 +21,7 @@ defmodule ContractsApi.LegalEntyti.Company do
     company
     |> cast(attrs, [:cnpj])
     |> cast_assoc(:addresses, with: &Address.changeset/2)
+    |> cast_assoc(:contracts, with: &Contract.changeset/2)
     |> validate_required([:cnpj])
   end
 end
--- lib/contracts_api/physical_person/person.ex
+++ lib/contracts_api/physical_person/person.ex
@@ -2,6 +2,7 @@ defmodule ContractsApi.PhysicalPerson.Person do
   use Ecto.Schema
   import Ecto.Changeset
   alias ContractsApi.Superscription.Address
+  alias ContractsApi.Covenant.Contract
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -11,6 +12,7 @@ defmodule ContractsApi.PhysicalPerson.Person do
     field :name, :string
 
     has_many :addresses, Address
+    has_many :contracts, Contract
 
     timestamps()
   end
@@ -20,6 +22,7 @@ defmodule ContractsApi.PhysicalPerson.Person do
     person
     |> cast(attrs, [:name, :cpf, :birth_date])
     |> cast_assoc(:addresses, with: &Address.changeset/2)
+    |> cast_assoc(:contracts, with: &Contract.changeset/2)
     |> validate_required([:name, :cpf, :birth_date])
   end
 end
