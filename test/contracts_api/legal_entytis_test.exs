4b09d2889a0978a111fa340d9581be64fd3f60e3
gissandrogama <gissandrogama@gmail.com>
2021-03-06 18:45:28 -0300

    chore(test): test legal entytis finished

--- test/contracts_api/legal_entytis_test.exs
+++ test/contracts_api/legal_entytis_test.exs
@@ -12,8 +12,27 @@ defmodule ContractsApi.LegalEntytisTest do
 
     test "return company error when parameters are in_valid" do
       assert {:error, %Ecto.Changeset{} = changeset} =
-        LegalEntytis.create_company(%{name: "", cnpj: "1245786000177"})
-        %{name: ["can't be blank"]} = errors_on(changeset)
+               LegalEntytis.create_company(%{name: "", cnpj: "1245786000177"})
+
+      %{name: ["can't be blank"]} = errors_on(changeset)
+    end
+  end
+
+  describe "get_company!" do
+    test "return a company when the id is valid" do
+      {:ok, %{id: id}} = LegalEntytis.create_company(%{name: "CLOE LTDA", cnpj: "1245786000177"})
+
+      {:ok, result} = LegalEntytis.get_company!(id)
+
+      assert result.name == "CLOE LTDA"
+      assert result.cnpj == "1245786000177"
+    end
+
+    test "Return error when id or company does not exist" do
+      LegalEntytis.create_company(%{name: "CLOE LTDA", cnpj: "1245786000177"})
+      id_invalid = "2af084fe-a625-44d1-b3f5-dda7000501a9"
+
+      assert LegalEntytis.get_company!(id_invalid) == {:error, :not_found}
     end
   end
 end
