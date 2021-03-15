6191adb9bc28f88e777926d03471a52cdd67953d
gissandrogama <gissandrogama@gmail.com>
2021-03-15 01:05:02 -0300

    feat(context): add context de contract chore(schema/contract): remove associations

--- lib/contracts_api/covenant/contract.ex
+++ lib/contracts_api/covenant/contract.ex
@@ -1,8 +1,8 @@
 defmodule ContractsApi.Covenant.Contract do
   use Ecto.Schema
   import Ecto.Changeset
-  alias ContractsApi.LegalEntytis.Company
-  alias ContractsApi.PhysicalPerson.Person
+  
+  alias ContractsApi.Part.Part
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -12,8 +12,7 @@ defmodule ContractsApi.Covenant.Contract do
     field :file_pdf, :string
     field :name, :string
 
-    belongs_to :company, Company
-    belongs_to :person, Person
+    has_many :parts, Part
 
     timestamps()
   end
@@ -21,9 +20,8 @@ defmodule ContractsApi.Covenant.Contract do
   @doc false
   def changeset(contract, attrs) do
     contract
-    |> cast(attrs, [:file_pdf, :name, :description, :date, :company_id, :person_id])
-    |> foreign_key_constraint(:company_id)
-    |> foreign_key_constraint(:person_id)
+    |> cast(attrs, [:file_pdf, :name, :description, :date])
+    |> cast_assoc(:parts, with: &Part.changeset/2)
     |> validate_required([:file_pdf, :name, :description, :date])
   end
 end
