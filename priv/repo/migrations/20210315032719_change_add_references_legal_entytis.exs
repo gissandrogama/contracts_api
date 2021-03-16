489c1434455d1215ce356c26579df0c1fd0323a3
gissandrogama <gissandrogama@gmail.com>
2021-03-16 02:46:34 -0300

    feat(migration): delete all migration feat(schema): delete schema part, contract, person and covenant

--- priv/repo/migrations/20210315032719_change_add_references_legal_entytis.exs
+++
@@ -1,9 +0,0 @@
-defmodule ContractsApi.Repo.Migrations.ChangeAddReferencesLegalEntytis do
-  use Ecto.Migration
-
-  def change do
-    alter table(:legal_entytis) do
-      add :part_id, references(:parts, on_delete: :nilify_all, type: :binary_id)
-    end
-  end
-end
--- priv/repo/migrations/20210315033119_change_add_references_physical_person.exs
+++
@@ -1,9 +0,0 @@
-defmodule ContractsApi.Repo.Migrations.ChangeAddReferencesPhysicalPerson do
-  use Ecto.Migration
-
-  def change do
-    alter table(:physical_persons) do
-      add :part_id, references(:parts, on_delete: :nilify_all, type: :binary_id)
-    end
-  end
-end
